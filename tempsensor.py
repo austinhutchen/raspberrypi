4dce62763ff24b4656ed129fa76e70a963cce88d
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-03-09 16:37:52 -0700

    accuracy of temp sensor improved

--- tempsensor.py
+++ tempsensor.py
@@ -3,16 +3,16 @@ import utime
 
 
 def main():
-    
     sensor_temp = machine.ADC(4)
-    conversion_factor= 3.3/ (65535)
-    i=0
-    temperature=0
-    while i<10:
-        
-        print('(',i+1,')','assessing data from your environment....')
-        temperature += round(27 -(sensor_temp.read_u16() * conversion_factor - 0.706)/0.0017212)
+    conversion_factor = 3.3 / (65535)
+    i = 0
+    temperature = 0
+    print('Loading....')
+    while i < 20:
+        print('(', i+1, ')','analyzing..')
+        temperature += round(27 - (sensor_temp.read_u16() * conversion_factor - 0.706)/0.0017212,2)
         utime.sleep(0.4)
-        i=i+1 
-    print('Current Temperature :' ,temperature/10)
+        i = i+1
+    print('Current Temperature :', temperature/10)
+
 main()
