9d7f4e2de6ae4ad8f2639629a489311983300f35
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-03-12 10:40:34 -0600

    news fetch works

--- news.py
+++ news.py
@@ -1,4 +1,4 @@
-
+## does a neqs fetch for the headlines
 import network  
 import urequests 
 
@@ -13,6 +13,14 @@ wlan.connect(ssid, password)
 
 
 
-print("1. Querying google.com:")
-r = urequests.get("http://www.google.com")
-print(r.content)
+print("Querying for news..")
+
+url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=919a8ccccf0f42fda7742752971a34c4'
+
+headers = {
+    'User-Agent': 'My User Agent 1.0',
+    'From': 'hutchenaustin@gmail.com'  # This is another valid field
+}
+
+response = urequests.get(url, headers=headers)
+print(response.text)
